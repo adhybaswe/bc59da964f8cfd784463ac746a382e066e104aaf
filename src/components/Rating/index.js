4e0a2ca74a71b4bf5d28f5ea3629d8816e295d9b
cahsibat <cah.sibat@gmail.com>
2020-03-04 00:35:01 +0700

    Add Component Rating

---
+++ src/components/Rating/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display:flex;
+  flex-direction:row;
+  color:#6e7679;
+  font-weight:500;
+`;
+
+const Star = styled.div`
+	display: inline-block;
+	font-size: 1.2rem;
+	line-height: 1;
+	
+	&:after {
+		margin-left:4px;
+		content: '★★★★★';
+		letter-spacing: 3px;
+		background: linear-gradient(90deg, #f9423a calc(${props => props.percent || "1"} / 5 * 100%), #eee  calc(${props => props.percent || "1"} / 5 * 100%) );
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	&:before {
+	  	content: '${props => props.percent || "1"}';
+	}
+`;
+
+
+const Rating = ({percent}) => {
+	return(
+		<Wrapper>
+			 <Star percent={ percent } />
+		</Wrapper>
+	)
+}
+
+export default Rating
