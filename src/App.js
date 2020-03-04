5ae791fdcefa16251ef20a6b44c477d204bc22e1
cahsibat <cah.sibat@gmail.com>
2020-03-04 08:12:45 +0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Calendar from './components/Calendar';
+import Rating from './components/Rating';
+import Button from './components/Button';
+import Product from './components/Product';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Calendar />
+      <Product 
+        title="Roasted Chicken with Scrambled Egg"  
+        image="http://localhost:3000/images/sample-1.jpg"  
+        category="Uptown Lunch"  
+        author="by Kulina"  
+        price={35000}  
+        rating={4.5}
+      />
+      <Product 
+        title="Roasted Chicken with Scrambled Egg"  
+        image="http://localhost:3000/images/sample-1.jpg"  
+        category="Uptown Lunch"  
+        author="by Kulina"  
+        price={35000}  
+        rating={4.5}
+      />
+      <Cart />
     </div>
   );
 }
