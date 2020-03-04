99ffbf5f6ca72b0ad95845ab311006b5bf773082
Adi Baswendra <adi.baswendra@technopartner.id>
2020-03-04 13:46:11 +0700

    Change color icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,10 +28,6 @@ const ButtonOpenModal = styled.a`
 	& > svg {
 		float:right;
 	}
-	path{
-			background-color:red;
-			color:red;
-		}
 `;
 
 
@@ -44,7 +40,7 @@ const Header = () => {
 			<Content>
 				Alamat Pengantaran
 				<ButtonOpenModal>
-					Tokopedia Tower <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+					Tokopedia Tower <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" fill="#f9423a"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 				</ButtonOpenModal>
 			</Content>
 		</Wrapper>
