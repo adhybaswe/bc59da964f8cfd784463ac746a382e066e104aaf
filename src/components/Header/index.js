1c0c86f48d2bffc1821c23e497882fe4b23b50ca
Adi Baswendra <adi.baswendra@technopartner.id>
2020-03-04 16:42:36 +0700

    Update Component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -41,7 +41,7 @@ const Container = styled.div`
   flex-direction:row;	
 `
 
-const Header = () => {
+const Header = ({onButtonClick}) => {
 	return(
 		<Wrapper>
 			<Container>
@@ -50,7 +50,7 @@ const Header = () => {
 				</ButtonBack>
 				<Content>
 					ALAMAT PENGANTARAN
-					<ButtonOpenModal>
+					<ButtonOpenModal onClick={onButtonClick}>
 						Tokopedia Tower <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" fill="#f9423a"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 					</ButtonOpenModal>
 				</Content>
